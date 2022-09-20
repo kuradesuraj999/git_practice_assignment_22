38957ca7760dc397f3141ac8d39eaa25fddd55f1
ASUS <abhiraskar@gmail.com>
2022-09-20 12:09:19 +0530

    updated prime logic

--- prime.js
+++ prime.js
@@ -1,10 +1,10 @@
-let num=13
+let num=27;
     count =0;
     for (i=1;i<=num;i++){
   if(num%i==0){
    count++;
   }
-} if(count==1){
+} if(count==2){
  console.log("Prime");
 }else{
   console.log("Not a prime");
